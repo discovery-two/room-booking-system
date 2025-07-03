5f062c29f7ebdd14abd354ac9715a02039651895
pluvivanto <140719273+pluvivanto@users.noreply.github.com>
2025-07-03 22:16:59 +0900

    Initial Spring Starter project commit

---
+++ src/main/java/win/pluvivanto/roombook/RoombookApplication.java
@@ -0,0 +1,13 @@
+package win.pluvivanto.roombook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoombookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoombookApplication.class, args);
+	}
+
+}
