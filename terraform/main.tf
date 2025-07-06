8b33629d1bc779a8d1562a9c9984d6ed790d9c4c
pluvivanto <140719273+pluvivanto@users.noreply.github.com>
2025-07-06 23:01:36 +0900

    feat: add Terraform IaC config for ECR

---
+++ terraform/main.tf
@@ -0,0 +1,77 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_ecr_repository" "ecr-repo" {
+  name                 = "${var.proj_name}-ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+data "aws_ecr_lifecycle_policy_document" "expire_policy" {
+  rule {
+    priority    = 1
+    description = "Keep only 3 most recent dev/test images"
+
+    action {
+      type = "expire"
+    }
+
+    selection {
+      tag_status      = "tagged"
+      tag_prefix_list = ["dev"]
+      count_type      = "imageCountMoreThan"
+      count_number    = 3
+    }
+  }
+
+  rule {
+    priority    = 2
+    description = "Keep only 2 most recent prod images"
+
+    action {
+      type = "expire"
+    }
+
+    selection {
+      tag_status      = "tagged"
+      tag_prefix_list = ["prod"]
+      count_type      = "imageCountMoreThan"
+      count_number    = 2
+    }
+  }
+
+  rule {
+    priority    = 3
+    description = "Expire untagged images after a day"
+
+    action {
+      type = "expire"
+    }
+
+    selection {
+      tag_status   = "untagged"
+      count_type   = "sinceImagePushed"
+      count_number = 1
+      count_unit   = "days"
+    }
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "ecr-lifecycle-policy" {
+  repository = aws_ecr_repository.ecr-repo.name
+
+  policy = data.aws_ecr_lifecycle_policy_document.expire_policy.json
+}
